ad8bcab62a93002f9fd14a0ba51cbe3c931de31d
swiss-chris <chris.dickinson@web.de>
2019-06-29 12:33:05 +0100

    more styling

--- src/components/Plateau.js
+++ src/components/Plateau.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import { isValidSize } from "../logic/validators";
-import { Jumbotron } from "react-bootstrap";
+import { Jumbotron, FormControl } from "react-bootstrap";
 
 const Plateau = props => {
   return (
     <Jumbotron>
       <h2>Plateau Size</h2>
       <p>How large is the plateau on Mars ?</p>
-      <input
+      <FormControl
         value={props.size}
         onChange={props.handleChange}
         placeholder="999 999"
